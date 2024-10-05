9747c7c7ec8a92e3ce3c373cdae61add241b4417
Max Schwarz <mschwa26@uvm.edu>
2024-10-05 17:49:33 -0400

    Basic Tab View

--- com.wruv.ljmayo.www/com.wruv.ljmayo.www/ContentView.swift
+++ com.wruv.ljmayo.www/com.wruv.ljmayo.www/ContentView.swift
@@ -9,13 +9,69 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            // Home Screen Tab
+            HomeScreen()
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    
+                }
+            
+            // Archives Screen Tab
+            ArchivesScreen()
+                .tabItem {
+                    Image(systemName: "line.3.horizontal")
+                        .rotationEffect(.degrees(90))
+                }
+            
+            // Account Screen Tab
+            AccountScreen()
+                .tabItem {
+                    Image(systemName: "person.fill")
+                }
         }
-        .padding()
+    }
+}
+
+struct HomeScreen: View {
+    var body: some View {
+        VStack {
+            Text("Home Screen")
+                .font(.largeTitle)
+                .padding()
+            Image(systemName: "house.fill")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .padding()
+        }
+    }
+}
+
+struct ArchivesScreen: View {
+    var body: some View {
+        VStack {
+            Text("Archives Screen")
+                .font(.largeTitle)
+                .padding()
+            Image(systemName: "line.3.horizontal")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .padding()
+        }
+    }
+}
+
+struct AccountScreen: View {
+    var body: some View {
+        VStack {
+            Text("Account Screen")
+                .font(.largeTitle)
+                .padding()
+            Image(systemName: "person.fill")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .padding()
+        }
     }
 }
 
