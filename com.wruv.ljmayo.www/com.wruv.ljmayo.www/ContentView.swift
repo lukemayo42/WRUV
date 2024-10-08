551f3f869ecac3418cec06db84b7c77507bd86ed
lukemayo422 <lukemayo422@gmail.com>
2024-10-07 22:36:44 -0400

    added play/pause button for radio stream at top of contentView

--- com.wruv.ljmayo.www/com.wruv.ljmayo.www/ContentView.swift
+++ com.wruv.ljmayo.www/com.wruv.ljmayo.www/ContentView.swift
@@ -8,27 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var playing = false
+    //placeholder values
+    @State private var showname = "radio show"
+    @State private var djName = "DJ"
     var body: some View {
-        TabView {
-            // Home Screen Tab
-            HomeScreen()
-                .tabItem {
-                    Image(systemName: "house.fill")
-                    
+        //could be a subview
+        ZStack(){
+            Rectangle().fill(.black).frame(height:110)
+            HStack{
+                
+                Button(action: toggleButton){
+                    ZStack{
+                        Circle()
+                            .fill(.white)
+                            .frame(height: 70)
+                        Circle()
+                            .fill(.black)
+                            .frame(height: 60)
+                        if playing{
+                            Image(systemName: "pause").foregroundColor(.white).font(.system(size: 35, weight: .bold, design: .rounded))
+                        }else{
+                            Image(systemName: "play").foregroundColor(.white).font(.system(size: 35, weight: .bold, design: .rounded))
+                        }
+                    }
                 }
+                HStack{
+                    Text("\(showname)\n \(djName)").foregroundColor(.white).bold().font(.largeTitle)
+                
+                }.padding()
+            }
+           
+        }
             
-            // Archives Screen Tab
-            ArchivesScreen()
-                .tabItem {
-                    Image(systemName: "line.3.horizontal")
-                        .rotationEffect(.degrees(90))
-                }
-            
-            // Account Screen Tab
-            AccountScreen()
-                .tabItem {
-                    Image(systemName: "person.fill")
-                }
+            TabView {
+                HomeScreen()
+                    .tabItem {
+                        Image(systemName: "house.fill")
+                        
+                    }
+                
+                // Archives Screen Tab
+                ArchivesScreen()
+                    .tabItem {
+                        Image(systemName: "line.3.horizontal")
+                            .rotationEffect(.degrees(90))
+                    }
+                
+                // Account Screen Tab
+                AccountScreen()
+                    .tabItem {
+                        Image(systemName: "person.fill")
+                    }
+            }
+        }
+    func toggleButton(){
+        if playing{
+            playing = false
+        }else{
+            playing = true
         }
     }
 }
