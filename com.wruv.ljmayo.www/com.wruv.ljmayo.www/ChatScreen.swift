f28e1d49e6e966b1d54bcee0f39c8d31de025ef4
John Driscoll <jsdriscoll8@gmail.com>
2024-10-20 10:34:46 -0700

    Added message struct, chat screen

---
+++ com.wruv.ljmayo.www/com.wruv.ljmayo.www/ChatScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ChatScreen.swift
+//  com.wruv.ljmayo.www
+//
+//  Created by user264313 on 10/20/24.
+//
+
+import SwiftUI
+
+struct ChatScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatScreen()
+}
---
+++ com.wruv.ljmayo.www/com.wruv.ljmayo.www/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  com.wruv.ljmayo.www
+//
+//  Created by user264313 on 10/20/24.
+//
+
+import Foundation
+
+struct Message {
+    let CHARLIMIT = 140
+    var text: String
+    var timeSent: Date
+}
