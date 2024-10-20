1bad6a0fd24f75d767513078176c3122c2fb3322
John Driscoll <jsdriscoll8@gmail.com>
2024-10-20 12:22:38 -0700

    Added messages to a scrollview and fixed message entry

--- com.wruv.ljmayo.www/com.wruv.ljmayo.www/ChatScreen.swift
+++ com.wruv.ljmayo.www/com.wruv.ljmayo.www/ChatScreen.swift
@@ -11,19 +11,28 @@ struct ChatScreen: View {
     @State private var newMessageText: String = ""
     @State var messages: [Message] = []
     var username: String
+    let CHATHEIGHT = 800.00
     
     var body: some View {
-        List {
-            ForEach(messages) { message in
-                Text("\(message.sendingUser): \(message.text)")
+        ScrollView {
+            VStack(alignment: .leading) {
+                List {
+                    ForEach(messages) { message in
+                        Text("\(message.sendingUser): \(message.text)")
+                    }
+                }
             }
+            .frame(height: CHATHEIGHT)
         }
+        .background(Color.white)
         
         TextField("Enter a new message here...", text: $newMessageText)
             .onSubmit {
                 var newMessage = Message(text: newMessageText, sendingUser: username, timeSent: Date())
                 messages.append(newMessage)
+                newMessageText = ""
             }
+            .frame(width: 350, alignment: .center)
     }
 }
 
