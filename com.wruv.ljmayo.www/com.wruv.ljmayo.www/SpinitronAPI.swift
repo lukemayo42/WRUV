7db8df7565114e894e728bc8a124c7b7b01e9a78
lukemayo422 <lukemayo422@gmail.com>
2024-11-01 18:59:54 -0400

    fetch function

--- com.wruv.ljmayo.www/com.wruv.ljmayo.www/SpinitronAPI.swift
+++ com.wruv.ljmayo.www/com.wruv.ljmayo.www/SpinitronAPI.swift
@@ -8,16 +8,16 @@
 import Foundation
 
 
-struct SpinWrapper:Codable{
+struct Spins:Decodable{
     var items : [Spin]
     
 }
 
-struct ShowWrapper: Codable{
+struct Shows: Decodable{
     var items: [Show]
 }
 
-struct Spin: Codable{
+struct Spin: Decodable{
     let image : String
     let artist : String
     let release : String
@@ -25,11 +25,46 @@ struct Spin: Codable{
     let genre: String
     let released: Int
     let song: String
+    let links: link
     
+    enum CodingKeys: String, CodingKey{
+        case image
+        case artist
+        case release
+        case label
+        case genre
+        case relaesed
+        case song
+        case links = "_links"
+    }
 }
 
-struct Show: Codable{
+struct link: Decodable{
+    let personas: personasLink
+}
+
+struct personasLink: Decodable{
+    let link: String
+    
+    enum CodingKeys: String, CodingKey{
+        link = "href"
+    }
+}
+
+struct Show: Decodable{
     let start:String
     let end: String
     let title: String
 }
+
+func fetchSpins() async throws -> Spins{
+    let spinsURL = URL(string:"https://spinitron.com/api/spins?access-token=api-key")
+    let (data, _) = try await URLSession.shared.data(from: spinsURL)
+    let spins = try JSONDecoder().decode(Wrapper.self, from: data)
+    return spins
+}
+Task{
+    try await fetchSpins()
+}
+
+
