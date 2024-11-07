52af727c8328b69b3ac26815d000497501b4f8cf
lukemayo422 <lukemayo422@gmail.com>
2024-11-07 13:16:31 -0500

    good http request

--- com.wruv.ljmayo.www/com.wruv.ljmayo.www/com_wruv_ljmayo_wwwApp.swift
+++ com.wruv.ljmayo.www/com.wruv.ljmayo.www/com_wruv_ljmayo_wwwApp.swift
@@ -10,7 +10,7 @@ import SwiftUI
 @main
 struct com_wruv_ljmayo_wwwApp: App {
     var style = UIStyles()
-    var radioStream = RadioStream(url:"http://icecast.uvm.edu:8005/wruv_fm_128.m3u")
+    var radioStream = RadioStream(url:"http://icecast.uvm.edu:8005/wruv_fm_128")
     var body: some Scene {
         WindowGroup {
             // loading screen appears first
