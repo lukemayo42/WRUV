6711197f1341fedf79e67ee7c9c4a63987550e90
Wade Bradford <wadebradford179@gmail.com>
2024-11-12 10:33:54 -0500

    User class works and displays on account screen after firebase registration/login

--- com.wruv.ljmayo.www/com.wruv.ljmayo.www/AccountScreen.swift
+++ com.wruv.ljmayo.www/com.wruv.ljmayo.www/AccountScreen.swift
@@ -7,19 +7,46 @@
 import SwiftUI
 
 struct AccountScreen: View {
+    @EnvironmentObject var authService: FirebaseAuthService
+    @State private var errorMessage: String?
+
     var body: some View {
-        VStack {
-            Text("Account Screen")
-                .font(.largeTitle)
-                .padding()
-            Image(systemName: "person.fill")
-                .resizable()
-                .frame(width: 100, height: 100)
-                .padding()
+        VStack(alignment: .leading) {
+            if let user = authService.currentUser {
+                Text("Welcome, \(user.chatName)")
+                    .font(.largeTitle)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .padding()
+                Text("Email: \(user.email)")
+                    .font(.title2)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .padding()
+            } else if let errorMessage = errorMessage {
+                Text(errorMessage)
+                    .foregroundColor(.red)
+                    .padding()
+            } else {
+                Text("No user information available")
+                    .font(.title)
+                    .padding()
+            }
+        }
+        .onAppear {
+            authService.fetchCurrentUser { error in
+                if let error = error {
+                    self.errorMessage = error
+                } else {
+                    self.errorMessage = nil
+                }
+            }
         }
     }
 }
 
+
 #Preview {
-    HomeView()
+    let mockAuthService = FirebaseAuthService()
+    mockAuthService.currentUser = User(email: "mockuser@example.com", chatName: "MockUser")
+    return AccountScreen()
+        .environmentObject(mockAuthService)
 }
