b75084be95424591a7e689e965b924578a9ee55a
Max Schwarz <mschwa26@uvm.edu>
2024-11-22 14:46:26 -0500

    Back to live button

--- com.wruv.ljmayo.www/com.wruv.ljmayo.www/RadioPlayerView.swift
+++ com.wruv.ljmayo.www/com.wruv.ljmayo.www/RadioPlayerView.swift
@@ -20,6 +20,14 @@ struct RadioPlayerView: View {
         ZStack{
             Rectangle().fill(style.black).frame(height:90)
             HStack{
+                HStack{
+                    Image(systemName: "chevron.left")
+                        .foregroundColor(.blue)
+                        .padding(.leading, 10)
+                    Text("Live").foregroundColor(.white)
+                        //.padding(.leading, 10)
+                }
+                Spacer()
                 Button(action: toggleButton){
                    ZStack{
                         Circle()
@@ -34,11 +42,15 @@ struct RadioPlayerView: View {
                             Image(systemName: "play").foregroundColor(style.white).font(.system(size: 32, weight: .bold, design: .rounded))
                         }
                     }
+                   .padding(.leading, 10)
                 }
+                Spacer()
                 HStack{
                     Text("\(spinitron.currShow.showName)\n \(spinitron.currShow.djName)").foregroundColor(.white).bold().font(style.primaryFont(size:24.0))
                 }
+                
             }
+            
         }.padding(EdgeInsets(top: 0, leading: 0, bottom: 10, trailing: 0))
     }
     func toggleButton(){
